f58b6eeed7753d61fe8513004170300658d08dc2
trankhuong96 <jeremiek96@gmail.com>
2017-10-16 14:04:15 +0700

    front-end creat

---
+++ ProjectManagement/Controllers/CreateController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class CreateController : Controller
+    {
+        // GET: Create
+        public ActionResult CreateStudent()
+        {
+            return View();
+        }
+
+        // GET: Create
+        public ActionResult CreateTeacher()
+        {
+            return View();
+        }
+
+        // GET: Create
+        public ActionResult CreateTopic()
+        {
+            return View();
+        }
+        // GET: Create
+        public ActionResult RegisterCapstone()
+        {
+            return View();
+        }
+    }
+}
